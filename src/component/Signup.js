231abea0b126ff7ad6c5ce93c3f8dfed9d76496c
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-28 17:32:37 +0200

    avec gestion du prop

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -3,8 +3,11 @@ import React, { Component } from "react"
 class Signup extends Component {
   constructor(props) {
     super(props)
+    let remplissage
+    if (typeof this.props.email !== undefined) remplissage = this.props.email
+    else remplissage = "monMailAMoi@serveur.fr"
     this.state = {
-      email: ""
+      email: remplissage
     }
     this.emailChange = this.emailChange.bind(this)
   }
@@ -17,7 +20,12 @@ class Signup extends Component {
     return (
       <div>
         <h1>{this.state.email}</h1>
-        <input type="email" name="email" onChange={this.emailChange} />
+        <input
+          type="email"
+          name="email"
+          placeholder={this.state.email}
+          onChange={this.emailChange}
+        />
       </div>
     )
   }
