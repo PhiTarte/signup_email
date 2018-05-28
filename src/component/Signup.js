dd8894037d65215899342a80b598f165b2d6de6f
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-28 16:24:15 +0200

    fix Signup component

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -3,12 +3,22 @@ import React, { Component } from "react"
 class Signup extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      email: ""
+    }
+    this.emailChange = this.emailChange.bind(this)
   }
+
+  emailChange(event) {
+    console.log(event.target.value)
+    this.setState({ email: event.target.value })
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.props.email}</h1>
-        <input type="email" name="email" />
+        <h1>{this.state.email}</h1>
+        <input type="email" name="email" onChange={this.emailChange} />
       </div>
     )
   }
